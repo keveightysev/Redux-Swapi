229c3c28d321b1f6b5d1dc37d1a655705d0673b9
Kevin Smith <keveightysev@gmail.com>
2019-03-06 14:36:14 -0600

    Built fetch start action and reducer case

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
@@ -6,3 +6,11 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const FETCH_CHAR_START = 'FETCH_CHAR_START';
+export const FETCH_CHAR_SUCCESS = 'FETCH_CHAR_SUCCESS';
+export const FETCH_CHAR_FAILURE = 'FETCH_CHAR_FAILURE';
+
+export const getChar = () => dispatch => {
+    dispatch({ type: FETCH_CHAR_START });
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_CHAR_START } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isLoading: false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,12 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHAR_START:
+      return {
+        ...state,
+        error: '',
+        isLoading: true
+      };
     default:
       return state;
   }
